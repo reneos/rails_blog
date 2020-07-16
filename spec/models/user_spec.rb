cdd54dbd28de1b7a227649a6d152c7cd0835738a
Nicole Wong <nrewong@gmail.com>
2020-07-16 18:28:41 +0900

    add username to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe User do
   context 'validations' do
     it 'ensures presence of email' do
       user = User.create
-      expect(user).to_not be_valid
+      expect(user.errors.messages[:email]).to include("can't be blank")
     end
 
     it 'ensures a valid email address' do
-      user = User.create(email: 'email', password: 'password')
-      expect(user).to_not be_valid
+      user = User.create(email: 'email')
+      expect(user.errors.messages[:email]).to include('is invalid')
+    end
+
+    it 'ensures a unique email address' do
+      user = User.create(email: 'test@test.com', password: 'password', username: 'user')
+      user2 = User.create(email: 'test@test.com')
+      expect(user2.errors.messages[:email]).to include('has already been taken')
     end
   end
 end
