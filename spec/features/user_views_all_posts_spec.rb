429cc85381830e703dcca2c181b0d8decac085c9
Nicole Wong <nrewong@gmail.com>
2020-07-18 00:24:31 +0900

    testing post index

--- spec/features/user_views_all_posts_spec.rb
+++ spec/features/user_views_all_posts_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'user can view index of posts' do
+  let(:user) { FactoryBot.create(:user) }
+
+  before(:each) do
+    login_as(user)
+  end
+
+  before(:all) do
+    create_many_published_posts(40, user)
+  end
+
   it 'only displays published posts' do
 
   end
--- spec/helpers/helpers.rb
+++ spec/helpers/helpers.rb
@@ -1,4 +1,19 @@
 module Helpers
+  def create_many_published_posts(quantity, user)
+    quantity.times do
+      Post.create(
+        user: user,
+        title: 'Blog Title',
+        content: 'This is a blog post',
+        is_published: true,
+        publish_date: Time.now - rand(0..3).days
+      )
+    end
+  end
+
+  def create_many_unpublished_posts(quantity, user)
+  end
+
   def create_unpublished_post
     visit new_post_path
     fill_in 'Title', with: 'Blog Title'
