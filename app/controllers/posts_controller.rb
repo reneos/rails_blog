db17123eae800aadd6d488e9494255c68782ac66
Nicole Wong <nrewong@gmail.com>
2020-07-19 18:39:07 +0900

    update devise authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :publish]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @posts = Post.published.paginate(:page => params[:page])
---
+++ spec/features/user_searches_for_posts_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'user can search through posts' do
+  it "can find results from a post's content" do
+
+  end
+
+  it "can find results from a post's title"
+
+  it 'displays a notice when no results are found'
+end
