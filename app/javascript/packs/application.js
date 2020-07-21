914e28bec341aa5cd05087f55c85b7c33808d510
Nicole Wong <nrewong@gmail.com>
2020-07-21 22:23:23 +0900

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// External imports
+
+
+// Turbolinks
+document.addEventListener('turbolinks:load', () => {
+});
