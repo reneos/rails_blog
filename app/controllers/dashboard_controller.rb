c80ece33eea6535e88f8e44dad9709eff32f390d
Nicole Wong <nrewong@gmail.com>
2020-07-22 04:11:58 +0900

    update dashboard layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
-    users_posts = PostPolicy::Scope.new(current_user, Post).resolve_dashboard
+    users_posts = PostPolicy::Scope.new(current_user, Post).resolve_dashboard.order(publish_date: :desc)
     @published_posts = users_posts.published
     @unpublished_posts = users_posts.unpublished
   end
